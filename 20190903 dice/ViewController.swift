fc88ad6e7ffdfb9dde231309ff2854c1eb62d868
michaelchen <namieamuro1978@outlook.com>
2019-09-03 19:09:08 +0800

    Initial Commit

---
+++ 20190903 dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  20190903 dice
+//
+//  Created by Michael Chen on 2019/9/3.
+//  Copyright © 2019 Michael Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
